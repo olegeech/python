0996a6970a464f5e491e055a66b8cb4aae61dd67
olegeech <tatarchyk.oleg@gmail.com>
2016-11-10 09:18:58 +0200

    Update sort.py

--- sort.py
+++ sort.py
@@ -1,5 +1,13 @@
 # python
 
-a_list[5, 8, 11, 3, 45]
+a_list = [5, 8, 11, 3, 45]
 
-for i in list:
+def bubble_sort(lst): 
+    for i in range(len(lst)):
+        for j in range(i+1, len(lst)):
+            if lst[j] < lst[i]:
+                lst[j], lst[i] = lst[i], lst[j]
+    return lst
+  
+  bubble_sort(a_list)
+  

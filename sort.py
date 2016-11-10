10c389a3cee588776eb0a8b4cbd6da1fbdda2e71
olegeech <tatarchyk.oleg@gmail.com>
2016-11-10 16:12:45 +0200

    Update sort.py

--- sort.py
+++ sort.py
@@ -2,17 +2,6 @@
 
 a_list = [5, 8, 11, 3, 45]
 
-def bubble_sort(lst): 
-    for i in range(len(lst)):
-        for j in range(i+1, len(lst)):
-            if lst[j] < lst[i]:
-                lst[j], lst[i] = lst[i], lst[j]
-    return lst
-  
-  bubble_sort(a_list)
-
-
-
 def insertion_sort(lst):
     for i in range(len(lst)):
         j = i
@@ -22,4 +11,14 @@ def insertion_sort(lst):
     return lst
 
 print(insertion_sort(a_list))
+
+
+def bubble_sort(lst): 
+    for i in range(len(lst)):
+        for j in range(i+1, len(lst)):
+            if lst[j] < lst[i]:
+                lst[j], lst[i] = lst[i], lst[j]
+    return lst
+  
+  bubble_sort(a_list)
   
